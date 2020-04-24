919c301f534c2a016df30c3692c8d58a2af49c8c
JAVIER <javi_hen@hotmail.com>
2020-04-24 19:40:08 -0400

    Actualizacion de dialogo

--- app/src/main/java/com/example/appcalificacion/dialogo_addCurso.java
+++ app/src/main/java/com/example/appcalificacion/dialogo_addCurso.java
@@ -3,6 +3,10 @@ package com.example.appcalificacion;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,10 +17,32 @@ public class dialogo_addCurso extends DialogFragment {
     @NonNull
     @Override
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        setCancelable(false);
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
 
-        builder.setView(getActivity().getLayoutInflater().inflate(R.layout.layout_crear_curso,null));
+        View view = getActivity().getLayoutInflater().inflate(R.layout.layout_crear_curso, null);
 
-        return super.onCreateDialog(savedInstanceState);
+        final EditText nom_curso = view.findViewById(R.id.et_nom_curso);
+        final EditText nom_materia = view.findViewById(R.id.et_nom_materia);
+        final TextView tvCancelar = view.findViewById(R.id.dialogo_cancelar);
+        final TextView tvGuardar = view.findViewById(R.id.dialogo_guardar);
+
+        tvGuardar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), "Guardando el curso", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+        tvCancelar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), "Estamos cerrando el formulario", Toast.LENGTH_SHORT).show();
+                dismiss();
+            }
+        });
+
+        builder.setView(view);
+        return builder.create();
     }
 }
