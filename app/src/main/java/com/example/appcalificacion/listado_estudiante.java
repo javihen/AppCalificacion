d5ec65808ca333db37b62d326b7257a19d611094
JAVIER <javi_hen@hotmail.com>
2020-04-25 18:09:09 -0400

    Actualizacion de listas

---
+++ app/src/main/java/com/example/appcalificacion/listado_estudiante.java
@@ -0,0 +1,14 @@
+package com.example.appcalificacion;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class listado_estudiante extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listado_estudiante);
+    }
+}
